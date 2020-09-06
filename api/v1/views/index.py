5afad70e046bb75841fc45360f3f968e2e2768e5
OctopusHugz <colsonscott53@gmail.com>
2020-09-06 21:50:39 +0000

    Added logic for returning stats from storage.count()

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ views routes """
+from models.user import User
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify
-from models import storage
 
 
 @app_views.route("/status")
@@ -17,11 +23,11 @@ def stats():
 def class_stats():
     """ endpoint that gets the stats of each obj """
     all_stats = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
     }
     return jsonify(all_stats)
