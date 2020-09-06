f4a6129eecf9025bf190ab0289a80e047b2a7a36
OctopusHugz <colsonscott53@gmail.com>
2020-09-06 22:06:43 +0000

    Added docstring to function

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ api """
-from flask import Flask
+from flask import Flask, make_response
 from os import getenv
-from flask import Blueprint
 from models import storage
 from api.v1.views import app_views
 app = Flask( __name__)
@@ -13,6 +12,12 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 def tear_down(self):
     """ calls close """
     storage.close()
+
+
+@app.errorhandler(404)
+def fourohfour(self):
+    """Returns a 404 error with JSON response"""
+    return make_response({"error": "Not found"}, 404)
 
 if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST"), port=getenv("HBNB_API_PORT"),
