f24c21648e91dd8a66360ed055aac3a8cb959211
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-06 21:36:52 +0000

    task 5 and code review

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,8 @@
 """ views routes """
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
+
 
 @app_views.route("/status")
 def stats():
@@ -10,3 +12,16 @@ def stats():
         "status": "OK"
     }
     return newdict
+
+@app_views.route("/stats")
+def class_stats():
+    """ endpoint that gets the stats of each obj """
+    all_stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(all_stats)
