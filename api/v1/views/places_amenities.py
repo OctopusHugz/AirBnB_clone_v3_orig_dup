dc477d0e30e4b764bb728a9454fdecc8b6062ffa
OctopusHugz <colsonscott53@gmail.com>
2020-09-08 22:00:00 +0000

    Fixed method routing and slightly altered structure

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -12,50 +12,49 @@ from api.v1.views import app_views
 from flask import jsonify, request
 
 
-@app_views.route("/places/<place_id>/amenities/<amenity_id>",
-                 strict_slashes=False, methods=['GET', 'POST'])
-def place_amenities(place_id, amenity_id):
+@app_views.route("/places/<place_id>/amenities/",
+                 strict_slashes=False, methods=['GET'])
+def place_amenities(place_id):
     """ /places/<place_id>/amenities route """
     amenity_list = []
     place = storage.get(Place, place_id)
-    amenity = storage.get(Amenity, amenity_id)
-    if place is not None:
-        if models.storage_t != 'db':
-            amenities = place.amenity_ids
-        else:
-            amenities = place.amenities
-        if request.method == 'GET':
-            for amen in amenities:
-                amenity_list.append(amen.to_dict())
-            return jsonify(amenity_list)
-        elif request.method == 'POST':
-            try:
-                if storage.get(Place, place_id) is None:
-                    return {"error": "Not found"}, 404
-                if storage.get(Amenity, amenity_id) is None:
-                    return {"error": "Not found"}, 404
-                if amenity_id in amenities:
-                    return jsonify(amenity.to_dict()), 200
-                return jsonify(amenity.to_dict()), 201
-            except:
-                return {"error": "Not a JSON"}, 400
-    return {"error": "Not found"}, 404
+    if place is None:
+        return {"error": "Not found"}, 404
+    if models.storage_t != 'db':
+        amenities = place.amenity_ids
+    else:
+        amenities = place.amenities
+    if request.method == 'GET':
+        for amenity in amenities:
+            amenity_list.append(amenity.to_dict())
+        return jsonify(amenity_list)
+
 
 
 @app_views.route("/places/<place_id>/amenities/<amenity_id>",
-                 strict_slashes=False, methods=['DELETE'])
+                 strict_slashes=False, methods=['DELETE', 'POST'])
 def place_amenity_id(place_id, amenity_id):
     """ /places/<place_id>/amenities/<amenity_id> route """
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
-    if place is not None:
-        if models.storage_t != 'db':
-            amenities = place.amenity_ids
-        else:
-            amenities = place.amenities
-        if amenity is not None and amenity in amenities:
-            if request.method == 'DELETE':
-                storage.delete(amenity)
-                storage.save()
-                return {}, 200
-    return {"error": "Not found"}, 404
+    if place is None or amenity is None:
+        return {"error": "Not found"}, 404
+    if models.storage_t != 'db':
+        amenities = place.amenity_ids
+    else:
+        amenities = place.amenities
+    if request.method == 'DELETE':
+        if amenity_id not in amenities:
+            return {"error": "Not found"}, 404
+        storage.delete(amenity)
+        storage.save()
+        return {}, 200
+    elif request.method == 'POST':
+        try:
+            if amenity_id in amenities:
+                return jsonify(amenity.to_dict()), 200
+            # Will this work for appending amenity in db and fs?
+            amenities.append(amenity)
+            return jsonify(amenity.to_dict()), 201
+        except:
+            return {"error": "Not a JSON"}, 400
