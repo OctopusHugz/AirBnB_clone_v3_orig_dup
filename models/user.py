6dcedb1640e3fd8ddbf9998ad52c2a55c48ce6ec
OctopusHugz <colsonscott53@gmail.com>
2020-09-11 04:35:49 +0000

    Changed password to hexdigest()

--- models/user.py
+++ models/user.py
@@ -28,5 +28,4 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-        if self.password:
-            self.password = hashlib.md5(self.password.encode())
+        self.password = hashlib.md5(self.password.encode()).hexdigest()
